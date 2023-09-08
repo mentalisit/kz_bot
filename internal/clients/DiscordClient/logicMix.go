6c0b601b1c1ed3da3d92119f868d2cbbe2a6ba40
mentalisit <mentalisit@gmail.com>
2023-09-08 04:42:51 +0300

    19.3.3

--- internal/clients/DiscordClient/logicMix.go
+++ internal/clients/DiscordClient/logicMix.go
@@ -310,9 +310,7 @@ func (d *Discord) SendToBridgeChatFilter(m *discordgo.MessageCreate, config mode
 		},
 		Config: config,
 	}
-	if m.MessageReference != nil &&
-		m.ReferencedMessage.Author != nil &&
-		m.ReferencedMessage.Author.String() != "" {
+	if m.ReferencedMessage != nil {
 		usernameR := m.ReferencedMessage.Author.String() //.Username
 		if m.ReferencedMessage.Member != nil && m.ReferencedMessage.Member.Nick != "" {
 			usernameR = m.ReferencedMessage.Member.Nick
