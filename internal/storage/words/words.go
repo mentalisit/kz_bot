f35e7b50bf55cc9c589d74822b258ebf11e27a17
mentalisit <mentalisit@gmail.com>
2024-01-13 06:07:44 +0200

    del drs

--- internal/storage/words/words.go
+++ internal/storage/words/words.go
@@ -1,18 +1,16 @@
 package words
 
 type Words struct {
-	dru map[string]string
-	ua  map[string]string
-	en  map[string]string
-	ru  map[string]string
+	ua map[string]string
+	en map[string]string
+	ru map[string]string
 }
 
 func NewWords() *Words {
 	w := &Words{
-		dru: make(map[string]string),
-		ua:  make(map[string]string),
-		en:  make(map[string]string),
-		ru:  make(map[string]string),
+		ua: make(map[string]string),
+		en: make(map[string]string),
+		ru: make(map[string]string),
 	}
 	w.setWords()
 
@@ -23,14 +21,11 @@ func (w *Words) setWords() {
 	w.setWordsUa()
 	w.setWordsRu()
 	w.setWordsEn()
-	w.setWordsDarkRu()
 
 }
 func (w *Words) GetWords(lang string, key string) string {
 	if lang == "ru" {
 		return w.ru[key]
-	} else if lang == "dru" {
-		return w.dru[key]
 	} else if lang == "ua" {
 		return w.ua[key]
 	} else if lang == "en" {
